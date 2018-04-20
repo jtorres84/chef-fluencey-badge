14410d5b696da46994110115a0d2b49c5fffa238
Jaciel <jaciel.torres@aspect.com>
2018-04-20 17:12:28 +0000

    added motd and changed apache default for node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,19 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #Install apache package
+
+if node['platform_family'] == "rhel"
+        package = "httpd"
+elsif node['platform_family'] == "debian"
+        package = "apache2"
+end
+
 package 'apache2' do
-        package_name 'httpd'
+        package_name package
         action :install 
 end
 
-serivce 'apache2' do
+service 'apache2' do
         service_name 'httpd'
         action [:start, :enable]
 end
